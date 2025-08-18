748bbafa345194cdc39a420ba9bb22cf9661ae48
Edwin De Nicolo <edn@oums-dlgpu1>
2025-08-18 21:27:26 +0100

    first training run

--- voltage_agent_testing/testing.py
+++ voltage_agent_testing/testing.py
@@ -1,6 +1,10 @@
 import torch
 
-from policy import RecurrentPPO
+#from policy import RecurrentPPO
+#from sb3_contrib import RecurrentPPO
+from policy import CustomAgentPolicy
+from policy import CustomRecurrentPPO
+
 from envs.qarray_2dot_env import QuantumDeviceEnv
 from model.voltage_agent import Agent
 
@@ -8,22 +12,37 @@ from model.voltage_agent import Agent
 def main():
     env = QuantumDeviceEnv()
     
-    model = RecurrentPPO(
-        "CustomAgentPolicy", 
-        env, 
+    # model = RecurrentPPO(
+    #     "CustomAgentPolicy", 
+    #     env, 
+    #     agent_class=Agent,
+    #     agent_kwargs={
+    #         'input_channels': 1, 
+    #         'action_dim': 2, 
+    #         'num_input_voltages': 2,
+    #     },
+    #     n_steps=128, # rollouts
+    #     batch_size=64,
+    #     use_wandb=False,
+    # )
+
+    model = CustomRecurrentPPO(
         agent_class=Agent,
         agent_kwargs={
-            'input_channels': 1, 
-            'action_dim': 2, 
+            'input_channels': 1,
+            'action_dim': 2,
             'num_input_voltages': 2,
         },
-        n_steps=128, # rollouts
-        batch_size=64,
-        use_wandb=False,
+        policy=CustomAgentPolicy,
+        env=env,
+        #n_steps=2048,
     )
     
-    print('Training ...')
-    model.learn(1000)
+    print('Learning ...')
+    model.learn(
+        total_timesteps=1000,
+        progress_bar=True,
+    )
 
 if __name__ == '__main__':
     main()
